b8b8906c11facd1ba8b71d15edda29a82e9d07f7
Boato Andrea <163646394+aandrea-boatoo@users.noreply.github.com>
2024-11-20 15:46:29 +0100

    overlay, margin problem

--- script/script.js
+++ script/script.js
@@ -17,11 +17,40 @@ axios.get(endPoint)
             const card = document.createElement('div');
             card.classList.add('card');
             card.innerHTML = `<img src="img/pin.svg" alt="pin" class="pin">
-            <figure>
-            <img src=${res.data[i].url} alt="foto-prova" class="foto bordo">
+            <figure id="${res.data[i].id}">
+            <img src=${res.data[i].url} alt="foto-prova" class="foto">
             <figcaption>${res.data[i].title}</figcaption>
             </figure>`
             container.appendChild(card);
         }
+        getFigures();
+    })
+    .catch((error) => {
+        console.log(error);
 
     })
+
+function getFigures() {
+    const cards = document.querySelectorAll("figure");
+    // console.log(cards);
+    cards.forEach(card => {
+        // console.log(card);
+        card.addEventListener('click', () => {
+            const overlay = document.getElementById('overlay');
+            const imgSelected = card.querySelector('img');
+            const captionSelected = card.querySelector('figcaption');
+            const imgOver = overlay.querySelector('img');
+            const closeButton = overlay.querySelector('button');
+            const overcaption = overlay.querySelector('p');
+            overlay.classList.remove('d-none');
+            closeButton.addEventListener('click', () => overlay.classList.add('d-none'));
+            imgOver.src = imgSelected.src;
+            overcaption.innerHTML = captionSelected.innerHTML
+
+
+        })
+
+    });
+
+}
+
